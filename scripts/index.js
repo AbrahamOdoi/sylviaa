b7b69c71fa497bf80d6bbc2e2bb01ab6146a22cd
Abraham Odoi <abrahamodoi2002@gmail.com>
2017-02-07 14:34:06 +0000

    Far

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,22 @@
 	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
 	function onDeviceReady() {
+		navigator.contactsPhoneNumbers.list(function(contacts) {
+			console.log(contacts.length + ' contacts found');
+			for (var i = 0; i < contacts.length; i++) {
+				console.log(contacts[i].id + " - " + contacts[i].displayName);
+				for (var j = 0; j < contacts[i].phoneNumbers.length; j++) {
+					var phone = contacts[i].phoneNumbers[j];
+					console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber + ")");
+					alert(phone);
+				}
+			}
+		}, function(error) {
+			console.error(error);
+		});
+
 		alert('My device is ready');
-		
+
 		document.getElementById("geolocationbtn").addEventListener("click", function() {
 			navigator.geolocation.getCurrentPosition(onSuccess, onError, {
 				enableHighAccuracy : true
