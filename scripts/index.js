a9360ef4f6fd33a8664aa4ecea2853583766ff93
Abraham Odoi <abrahamodoi2002@gmail.com>
2017-02-07 15:15:08 +0000

    Sdfj

--- scripts/index.js
+++ scripts/index.js
@@ -2,25 +2,25 @@
 	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
 	function onDeviceReady() {
-		navigator.contactsPhoneNumbers.list(function(contacts) {
-			console.log(contacts.length + ' contacts found');
-			for (var i = 0; i < contacts.length; i++) {
-				console.log(contacts[i].id + " - " + contacts[i].displayName);
-				for (var j = 0; j < contacts[i].phoneNumbers.length; j++) {
-					var phone = contacts[i].phoneNumbers[j];
-					console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber + ")");
-					$("#displaycontact").
-				}
-			}
-		}, function(error) {
-			console.error(error);
-		});
-
 		alert('My device is ready');
 
 		document.getElementById("geolocationbtn").addEventListener("click", function() {
-			navigator.geolocation.getCurrentPosition(onSuccess, onError, {
-				enableHighAccuracy : true
+			// navigator.geolocation.getCurrentPosition(onSuccess, onError, {
+			// enableHighAccuracy : true
+			// });
+
+			navigator.contactsPhoneNumbers.list(function(contacts) {
+				// console.log(contacts.length + ' contacts found');
+				for (var i = 0; i < contacts.length; i++) {
+					// console.log(contacts[i].id + " - " + contacts[i].displayName);
+					for (var j = 0; j < contacts[i].phoneNumbers.length; j++) {
+						var phone = contacts[i].phoneNumbers[j];
+						// console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber + ")");
+						$("#displaycontact").html("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber + ")");
+					}
+				}
+			}, function(error) {
+				console.error(error);
 			});
 
 		});
